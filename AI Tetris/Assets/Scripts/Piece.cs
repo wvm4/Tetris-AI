0a3c7f6eb4d07a16fb8b185305f825ad958c7213
Jispy05 <112877276+Jispy05@users.noreply.github.com>
2022-11-20 18:18:06 +0100

    Movement of pieces(faulty)
    Zorgt ervoor dat de pieces kunnen moven en collision herkennen (ergens zit een fout, HELP)

--- AI Tetris/Assets/Scripts/Piece.cs
+++ AI Tetris/Assets/Scripts/Piece.cs
@@ -21,4 +21,31 @@ public class Piece : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        this.board.Clear(this);
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+            Move(Vector2Int.left);
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+            Move(Vector2Int.right);
+        }
+
+        this.board.Set(this);
+    }
+
+    private bool Move(Vector2Int translation)
+    {
+        Vector3Int newPosition = this.position;
+        newPosition.x += translation.x;
+        newPosition.y += translation.y;
+
+        bool valid = this.board.IsValidPosition(this, newPosition);
+
+        if (valid) {
+            this.position = newPosition;
+        }
+        return valid;
+    }
+
 }
